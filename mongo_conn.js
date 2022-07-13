fb6ad1877911d701cbeed576aaa268131a88d5f0
akshay shinde <akshayshinde7289@gmail.com>
2022-07-13 19:43:59 +0530

    mongo_conn

--- mongo_conn.js
+++ mongo_conn.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 
 //connection to the database
 const connectDB = async () => {
-  await mongoose.connect("mongodb://localhost:27017/todaysRecipe", { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-  })
-  console.log("connection successful!");
+  try{
+    mongoose.connect("mongodb://localhost:27017/todaysRecipe", { 
+      useNewUrlParser: true, 
+      useUnifiedTopology: true 
+    })
+    console.log("connection successful!");
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 module.exports = connectDB;
