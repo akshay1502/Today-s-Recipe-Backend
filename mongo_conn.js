01d1e35383e59d54dc15586fcc6843187364c581
akshay shinde <akshayshinde7289@gmail.com>
2022-07-14 17:13:51 +0530

    db setup dev mode

--- mongo_conn.js
+++ mongo_conn.js
@@ -3,11 +3,10 @@ const mongoose = require('mongoose');
 //connection to the database
 const connectDB = async () => {
   try{
-    mongoose.connect("mongodb+srv://akshay1502:akshayrs@cluster0.y8buk.mongodb.net/?retryWrites=true&w=majority", { 
+    mongoose.connect(process.env.DBURL, { 
       useNewUrlParser: true, 
       useUnifiedTopology: true 
-    })
-    console.log("connection successful!");
+    }).then(() => console.log("connection successful!"));
   } catch(err) {
     console.log(err);
   }
--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const connectDB = require('./mongo_conn');
 connectDB();
 
 const authRoute = require('./routes/auth');
-
+console.log(process.env.DBURL);
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 app.use(cors({
   credentials: true,
-  origin: ["https://todaysrecipebackend.herokuapp.com", "http://localhost:5000"]
+  origin: ["https://todaysrecipe.herokuapp.com/", "http://localhost:3000"]
 }));
 app.use(cookieParser());
 app.use("/recipes", require('./routes/recipes'));
 app.use("/users", require('./routes/users'));
 app.use(authRoute);
 app.get('/', (req, res) => res.json({ message: 'Server running' }));
-app.listen(process.env.PORT || 5000, () => console.log('server running on port'));
+app.listen(process.env.PORT || 5000, () => console.log('server running'));
