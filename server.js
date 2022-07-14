89f8b48ab2f330c2c28ee9ee8286a74fd0d1cd10
akshay shinde <akshayshinde7289@gmail.com>
2022-07-14 23:20:57 +0530

    origin typo fix

--- server.js
+++ server.js
@@ -12,12 +12,8 @@ app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 app.use(cors({
   credentials: true,
-  origin: ["https://todaysrecipe.herokuapp.com/", "http://localhost:3000"]
+  origin: ["https://todaysrecipe.herokuapp.com", "http://localhost:3000"]
 }));
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://todaysrecipe.herokuapp.com/');
-  next();
-})
 app.use(cookieParser());
 app.use("/recipes", require('./routes/recipes'));
 app.use("/users", require('./routes/users'));
