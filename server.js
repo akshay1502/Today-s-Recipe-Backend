7cecd52996fd0e8e9f974bc2429fca35c09478be
akshay shinde <akshayshinde7289@gmail.com>
2022-07-22 15:35:48 +0530

    vercel folder structure

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const connectDB = require('./mongo_conn');
 connectDB();
 
-const authRoute = require('./routes/auth');
+const authRoute = require('./api/auth');
 console.log(process.env.DBURL);
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
@@ -15,8 +15,8 @@ app.use(cors({
   origin: ["https://todaysrecipe.herokuapp.com", "http://localhost:3000"]
 }));
 app.use(cookieParser());
-app.use("/recipes", require('./routes/recipes'));
-app.use("/users", require('./routes/users'));
+app.use("/recipes", require('./api/recipes'));
+app.use("/users", require('./api/users'));
 app.use(authRoute);
 app.get('/', (req, res) => res.json({ message: 'Server running' }));
 app.listen(process.env.PORT || 5000, () => console.log('server running'));
