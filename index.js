26fd56d070f8174d1a372ccb34bbbfc3f0d19e6c
akshay shinde <akshayshinde7289@gmail.com>
2022-07-23 15:40:45 +0530

    vercel setup 5

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const connectDB = require('./mongo_conn');
+connectDB();
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.json("test");
-});
-app.get("/recipes", (req, res) => {
-  res.json("get recipes");
-});
-
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server Running on port ${port}`));
+const authRoute = require('./api/auth');
+console.log(process.env.DBURL);
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ extended: true, limit: '30mb' }));
+app.use(cors({
+  credentials: true,
+  origin: ["https://todaysrecipe.herokuapp.com", "http://localhost:3000"]
+}));
+app.use(cookieParser());
+app.use("/recipes", require('./api/recipes'));
+app.use("/users", require('./api/users'));
+app.use(authRoute);
+app.get('/', (req, res) => res.json({ message: 'Server running' }));
+app.listen(process.env.PORT || 5000, () => console.log('server running'));
--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors');
-// const cookieParser = require('cookie-parser');
-// require('dotenv').config();
-// const connectDB = require('./mongo_conn');
-// connectDB();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const connectDB = require('./mongo_conn');
+connectDB();
 
-// const authRoute = require('./api/auth');
-// console.log(process.env.DBURL);
-// app.use(express.json({ limit: '30mb' }));
+const authRoute = require('./api/auth');
+console.log(process.env.DBURL);
+app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
-// app.use(cors({
-//   credentials: true,
-//   origin: ["https://todaysrecipe.herokuapp.com", "http://localhost:3000"]
-// }));
-// app.use(cookieParser());
-// app.use("/recipes", require('./api/recipes'));
-// app.use("/users", require('./api/users'));
-// app.use(authRoute);
+app.use(cors({
+  credentials: true,
+  origin: ["https://todaysrecipe.herokuapp.com", "http://localhost:3000"]
+}));
+app.use(cookieParser());
+app.use("/recipes", require('./api/recipes'));
+app.use("/users", require('./api/users'));
+app.use(authRoute);
 app.get('/', (req, res) => res.json({ message: 'Server running' }));
 app.listen(process.env.PORT || 5000, () => console.log('server running'));
